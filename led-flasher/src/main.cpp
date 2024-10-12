bf2f967c871e557701658b5c4fea38eda64effef
Martin Harris <1488244+nakomis@users.noreply.github.com>
2024-10-12 23:04:47 +0100

    Adds options for local (mosquitto) MQTT

--- led-flasher/src/main.cpp
+++ led-flasher/src/main.cpp
@@ -8,7 +8,14 @@
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
 
-WiFiClientSecure net = WiFiClientSecure();
+#define LOCAL_MQTT
+
+#ifdef LOCAL_MQTT
+    WiFiClient net = WiFiClient();
+#else
+    WiFiClientSecure net = WiFiClientSecure();
+#endif
+
 MQTTClient client = MQTTClient(256);
 
 void messageHandler(String& topic, String& payload) {
@@ -25,20 +32,33 @@ void connectAWS() {
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.println("Connecting to Wi-Fi");
+
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
+    client.setKeepAlive(31);
+
+#ifdef LOCAL_MQTT
+    // Connect to the MQTT broker on the AWS endpoint we defined earlier
+    client.begin(LOCAL_MQTT_HOSTNAME, 8883, net);
+#else
     // Configure WiFiClientSecure to use the AWS IoT device credentials
     net.setCACert(AWS_CERT_CA);
     net.setCertificate(AWS_CERT_CRT);
     net.setPrivateKey(AWS_CERT_PRIVATE);
     // Connect to the MQTT broker on the AWS endpoint we defined earlier
     client.begin(AWS_IOT_ENDPOINT, 8883, net);
+#endif
+    
     // Create a message handler
     client.onMessage(messageHandler);
     Serial.print("Connecting to AWS IOT");
+#ifdef LOCAL_MQTT
+    while (!client.connect(THINGNAME, LOCAL_MQTT_USERNAME, LOCAL_MQTT_PASSWORD)) {
+#else
     while (!client.connect(THINGNAME)) {
+#endif
         Serial.print(".");
         delay(100);
     }
